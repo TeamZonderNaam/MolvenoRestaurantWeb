c39ccfc5d5687399d4c88460907dcd351bc3d81b
Robin de Jong <robin.jong@capgemini.com>
2018-09-25 16:05:20 +0200

    Added test for findAll for ingredients

--- src/test/java/com/capgemini/molveno/repository/IngredientRepositoryTest.java
+++ src/test/java/com/capgemini/molveno/repository/IngredientRepositoryTest.java
@@ -7,6 +7,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(JUnit4.class)
@@ -41,16 +44,25 @@ public class IngredientRepositoryTest {
         ingredient1.setPricePerUnit(1.0);
         ingredient1.setUnit(gramUnit);
 
-        Ingredient created = repository.save(ingredient1);
-        assertThat(created.getId()).isEqualTo(0);
+        Ingredient created1 = repository.save(ingredient1);
+        assertThat(created1.getId()).isEqualTo(0);
 
         Ingredient ingredient2 = new Ingredient();
         ingredient2.setName("Name 1");
         ingredient2.setPricePerUnit(1.0);
         ingredient2.setUnit(gramUnit);
 
-        created = repository.save(ingredient2);
-        assertThat(created.getId()).isEqualTo(1);
+        Ingredient created2 = repository.save(ingredient2);
+        assertThat(created2.getId()).isEqualTo(1);
+
+
+        Iterable<Ingredient> iterableAll = repository.findAll();
+        List<Ingredient> all = new LinkedList<>();
+        iterableAll.forEach(all::add);
+
+        assertThat(all.size()).isEqualTo(2);
+        assertThat(all.get(0).getId()).isEqualTo(created1.getId());
+        assertThat(all.get(1).getId()).isEqualTo(created2.getId());
     }
 
     @Test
