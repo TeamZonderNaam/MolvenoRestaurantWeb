a9f4b3228e5c7b5e0b1708491722cc78ca3a465d
Robin de Jong <robin.jong@capgemini.com>
2018-09-27 16:19:50 +0200

    Added the ingredients CRUD to the Menuitem view

--- src/main/java/com/capgemini/molveno/controller/IngredientController.java
+++ src/main/java/com/capgemini/molveno/controller/IngredientController.java
@@ -6,6 +6,7 @@ import com.capgemini.molveno.service.IngredientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
