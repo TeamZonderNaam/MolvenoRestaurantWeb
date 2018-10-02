fe8058383e0a9c66b32b6ef7967cfa9d858e9e15
Robin de Jong <robin.jong@capgemini.com>
2018-10-02 15:09:32 +0200

    Added a line explaining why .on() was used instead of .click()

--- src/main/resources/static/js/menu/create.js
+++ src/main/resources/static/js/menu/create.js
@@ -7,6 +7,9 @@ $(function() {
     };
 
     var modal = $("#item-modal");
+    // We can't use the default click function, because the data is loaded in dynamically.
+    // The following line lets jQuery listen on all clicks on the body and only filter out those whose
+    // selector matches the one that was clicked on. This allows us to get click events on dynamic content.
     $("body").on("click", ".save", function() {
         var form = modal.find("form");
         var model = FormUtil.formToValues(pairs, form);
