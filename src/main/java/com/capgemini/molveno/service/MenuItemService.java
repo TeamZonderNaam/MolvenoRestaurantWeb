528e8effcb1894cd2737bb26ebeeefccfec87187
Robin de Jong <robin.jong@capgemini.com>
2018-10-03 16:02:28 +0200

    Started working on the menuitem and order refactor

--- src/main/java/com/capgemini/molveno/service/MenuItemService.java
+++ src/main/java/com/capgemini/molveno/service/MenuItemService.java
@@ -23,67 +23,6 @@ public class MenuItemService {
         MenuItem created = this.menuItemRepository.save(menuItem);
         return created.getId();
     }
-
-    @PostConstruct
-    public void addSomeItemsToGetStarted() {
-
-//        for(int i = 1; i < 6; i++) {
-//            MenuItem item = new MenuItem();
-//            item.setCategory("Food");
-//            menuItemRepository.save(item);
-//        }
-        MenuItem item1 = this.read(41);
-        item1.setName("Fresh Meat Dumpling");
-        item1.setPrice(3.5);
-        item1.setNumber(1);
-        menuItemRepository.save(item1);
-
-        MenuItem item2 = this.read(42);
-        item2.setName("Fried Wonton");
-        item2.setPrice(4);
-        item2.setNumber(2);
-        menuItemRepository.save(item2);
-
-        MenuItem item3 = this.read(43);
-        item3.setName("Beancurd Soup");
-        item3.setPrice(4);
-        item3.setNumber(2);
-        menuItemRepository.save(item3);
-
-        MenuItem item4 = this.read(44);
-        item4.setName("Peking Duck");
-        item4.setPrice(4);
-        item4.setNumber(2);
-        menuItemRepository.save(item4);
-
-        MenuItem item5 = this.read(45);
-        item5.setName("Hot Plate");
-        item5.setPrice(4);
-        item5.setNumber(2);
-        menuItemRepository.save(item5);
-
-//        this.read(1).setName("Fresh Meat Dumpling");
-//        this.read(2).setName("Fried Wonton");
-//        this.read(3).setName("Beancurd Soup");
-//        this.read(4).setName("Peking Duck");
-//        this.read(5).setName("Hot Plate");
-//        this.read(1).setPrice(3.5);
-//        this.read(2).setPrice(4);
-//        this.read(3).setPrice(4.5);
-//        this.read(4).setPrice(14.5);
-//        this.read(5).setPrice(21.5);
-//        this.read(1).setNumber(1);
-//        this.read(2).setNumber(2);
-//        this.read(3).setNumber(15);
-//        this.read(4).setNumber(23);
-//        this.read(5).setNumber(45);
-
-//        for(int i = 41; i < 43; i++) {
-//            MenuItem item = this.read(i);
-//            menuItemRepository.save(item);
-//        }
-    }
-
     public List<MenuItem> all(){
         Iterable<MenuItem> source = this.menuItemRepository.findAll();
         List<MenuItem> target = new ArrayList<MenuItem>();
