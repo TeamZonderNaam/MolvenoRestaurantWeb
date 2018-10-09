f5159d51892a60116d778fe0fd50634a4e7b4f9d
Tim Neefjes <tim.neefjes@capgemini.com>
2018-10-09 14:27:16 +0200

    Added validations

--- src/main/resources/static/js/table/create.js
+++ src/main/resources/static/js/table/create.js
@@ -6,20 +6,39 @@ $(function() {
     // The following line lets jQuery listen on all clicks on the body and only filter out those whose
     // selector matches the one that was clicked on. This allows us to get click events on dynamic content.
     $("body").on("click", ".save", function() {
-        $('#error').empty();
+        clearErrors();
         var model = FormUtil.formToValues(DATA_PAIRS, form);
+
+        // Check the user input
+        var error = false;
+        if (model.number < 0 || model.number == "" || !((model.number-Math.floor(model.number))==0)) {
+            $('#errorNumber').empty().append("Invalid table number");
+            error = true;
+        }
+        if (model.numberOfPersons <= 0 || !((model.numberOfPersons-Math.floor(model.numberOfPersons))==0)) {
+            $('#errorCapacity').empty().append("Invalid capacity");
+            error = true;
+        }
+        if (error == true) {
+            throw "InvalidInput";
+        }
         URLUtil.post(BASE_URL, model).then(function(obj) {
             DATA_TABLE.row.add(obj).draw(false);
             modal.modal('toggle');
             FormUtil.emptyForm(form);
         }, function(XMLHttpRequest, textStatus, errorThrown) {
             var error = JSON.parse(XMLHttpRequest.responseText); //if this is JSON otherwise just alerting XMLHttpRequest.responseText will do
-            $('#error').empty().append(error.message);
+            $('#errorNumber').empty().append(error.message);
         });
     }).on("click", ".add-item", function() {
-        $('#error').empty();
+        clearErrors();
         // It's possible that a new value will be added after editing.
         // Editing an item sets the edit state, so we need to revert it back if that's the case
         FormUtil.makeFormSave(form);
     });
 });
+
+function clearErrors() {
+    $('#errorNumber').empty();
+    $('#errorCapacity').empty();
+}
