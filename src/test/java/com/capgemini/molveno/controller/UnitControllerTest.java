3c5c8dd2d0d7bfc531ac18b3aa92877516f28ec6
Robin de Jong <robin.jong@capgemini.com>
2018-10-09 14:16:32 +0200

    Added the tests for the UnitController

--- src/test/java/com/capgemini/molveno/controller/UnitControllerTest.java
+++ src/test/java/com/capgemini/molveno/controller/UnitControllerTest.java
@@ -3,6 +3,8 @@ package com.capgemini.molveno.controller;
 import com.capgemini.molveno.controller.api.UnitController;
 import com.capgemini.molveno.model.Unit;
 import com.capgemini.molveno.repository.UnitRepository;
+import com.capgemini.molveno.service.UnitService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,12 +20,14 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
@@ -33,7 +37,7 @@ public class UnitControllerTest {
     private UnitController controller;
 
     @Mock
-    private UnitRepository unitRepository;
+    private UnitService unitService;
 
     private List<Unit> units;
 
@@ -49,13 +53,14 @@ public class UnitControllerTest {
 
         this.mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
 
-        when(unitRepository.findAll()).thenReturn(units);
+        when(unitService.all()).thenReturn(units);
 
-        when(unitRepository.findById(Mockito.intThat(id -> id == 0))).thenReturn(Optional.of(units.get(0)));
-        when(unitRepository.findById(Mockito.intThat(id -> id == 1))).thenReturn(Optional.of(units.get(0)));
-        when(unitRepository.findById(Mockito.intThat(id -> id > 1))).thenReturn(Optional.empty());
+        when(unitService.read(Mockito.intThat(id -> id == 0))).thenReturn(units.get(0));
+        when(unitService.read(Mockito.intThat(id -> id == 1))).thenReturn(units.get(1));
+        when(unitService.read(Mockito.intThat(id -> id > 1))).thenReturn(null);
 
-        when(unitRepository.save(Mockito.any(Unit.class))).then(AdditionalAnswers.returnsFirstArg());
+        when(unitService.create(Mockito.any(Unit.class))).thenReturn(0);
+        when(unitService.update(Mockito.any(Unit.class))).then(AdditionalAnswers.returnsFirstArg());
     }
 
     @Test
@@ -68,6 +73,60 @@ public class UnitControllerTest {
                 .andExpect(jsonPath("$[1].id").value(units.get(1).getId()))
                 .andExpect(jsonPath("$[1].name").value(units.get(1).getName()))
                 .andExpect(jsonPath("$[2].id").value(units.get(2).getId()))
-                .andExpect(jsonPath("$[2].name").value(units.get(2).getName()));
+                .andExpect(jsonPath("$[2].name").value(units.get(2).getName()))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void getSingleTest() throws Exception {
+        this.mockMvc.perform(
+            get("/api/unit/1").contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(jsonPath("$.id").value(units.get(1).getId()))
+                .andExpect(jsonPath("$.name").value(units.get(1).getName()))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void postTest() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(units.get(0));
+
+        this.mockMvc.perform(
+                post("/api/unit/")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(json))
+                .andDo(print())
+                .andExpect(jsonPath("$.id").value(units.get(0).getId()))
+                .andExpect(jsonPath("$.name").value(units.get(0).getName()))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void putTest() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(units.get(0));
+
+        this.mockMvc.perform(
+                put("/api/unit/0")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andDo(print())
+                .andExpect(jsonPath("$.id").value(units.get(0).getId()))
+                .andExpect(jsonPath("$.name").value(units.get(0).getName()))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void deleteTest() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(units.get(0));
+
+        this.mockMvc.perform(
+                delete("/api/unit/0")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 }
