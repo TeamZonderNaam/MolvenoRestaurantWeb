60727496a46bf26f09c73442b5368a5975d6bc3f
Tim Neefjes <tim.neefjes@capgemini.com>
2018-10-11 02:24:09 +0200

    Can now check for free tables.

--- src/main/java/com/capgemini/molveno/repository/TimeslotRepository.java
+++
@@ -1,7 +0,0 @@
-package com.capgemini.molveno.repository;
-
-import com.capgemini.molveno.model.Timeslot;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TimeslotRepository extends CrudRepository<Timeslot, Integer> {
-}
