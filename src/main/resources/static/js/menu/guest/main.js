bd3b601bc1f5a08013649b70d1e84bb2dd8b7fa2
Robin de Jong <robin.jong@capgemini.com>
2018-10-11 10:14:22 +0200

    Started writing the template for the menu item

--- src/main/resources/static/js/menu/guest/main.js
+++ src/main/resources/static/js/menu/guest/main.js
@@ -2,5 +2,24 @@ var BASE_URL = "/api/menuItem/";
 $(function() {
     URLUtil.get(BASE_URL).then(function(arr) {
         console.log("Arr: ", arr);
+        // Create buckets of arrays based on the categories.
+        var bucket = sortToBuckets(arr);
+        console.log(bucket);
     });
+
+
+    function sortToBuckets(arr) {
+        var buckets = {};
+        $.each(arr, function(i, obj) {
+            var category = obj.category.name;
+            // There wasn't an occurrence of this category yet
+            if (buckets[category] === undefined) {
+                buckets[category] = [];
+            }
+
+            buckets[category].push(obj);
+        });
+
+        return buckets;
+    }
 });
